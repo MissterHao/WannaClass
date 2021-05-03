0120e2165d31406f3808e364de45e2d6fec791df
Hao Wei Li <henryliking@gmail.com>
2021-05-04 02:58:29 +0800

    feat: init version of landing page and login block done

--- renderer/js/landing.js
+++ renderer/js/landing.js
@@ -1,6 +1,16 @@
 const cta = document.querySelector(".login-panel .cta")
 
 cta.onclick = function(){
-    document.querySelector(".login-panel .hero-image").classList.toggle("move-to-hidden")
-    document.querySelector(".login-panel .hero-text").classList.toggle("move-to-hidden")
+    document.querySelector(".login-panel .hero-image").classList.toggle("move-to-hidden");
+    document.querySelector(".login-panel .hero-text").classList.toggle("move-to-hidden");
+    document.querySelector(".login-panel .login-block").classList.toggle("move-to-hidden");
+
+    if(cta.querySelector("i").classList.contains("fa-arrow-right")){
+        cta.querySelector("span").textContent = "返回首頁"
+    }else {
+        cta.querySelector("span").textContent = "開始使用"
+    }
+    cta.querySelector("i").classList.toggle("fa-arrow-right")
+    cta.querySelector("i").classList.toggle("fa-arrow-left")
+
 }
