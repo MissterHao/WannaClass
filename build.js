e0adde171835630855dc0fcdfa9ba7242d9dd9b6
Hao Wei Li <henryliking@gmail.com>
2021-05-14 22:09:58 +0800

    feat: Build js with electron builder added

---
+++ build.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const builder = require('electron-builder');
+
+builder.build({
+
+    projectDir: path.resolve(__dirname),  // 專案路徑 
+
+    win: ['nsis', 'portable'],  // nsis . portable
+    config: {
+        "appId": "com.missterhao.wannaclass",
+        "productName": "Wanna Class", // 應用程式名稱 ( 顯示在應用程式與功能 )
+        "directories": {
+            "output": "build/win"
+        },
+        "win": {
+            // "icon": path.resolve(__dirname, 'cat.png'),
+        }
+    },
+})
+    .then(
+        data => console.log(data),
+        err => console.error(err)
+    );
